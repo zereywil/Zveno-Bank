beb8241f5961b3ba5c7ba99135d3e47cf00b0d2d
Ahmarov Daniel <axmarov06@bk.ru>
2026-05-04 18:49:53 +0500

    pattern

--- app/src/main/java/com/example/zveno_bank/MainActivity.kt
+++ app/src/main/java/com/example/zveno_bank/MainActivity.kt
@@ -1,20 +1,34 @@
-package com.example.zveno_bank
+package com.example.zveno_bank // Замени на свое название пакета
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Находим наши кнопки по айди
+        val payButton = findViewById<Button>(R.id.btn_pay)
+        val transferButton = findViewById<Button>(R.id.btn_transfer)
+
+        // Вешаем слушатель нажатий
+        payButton.setOnClickListener {
+            Toast.makeText(this, "Оплата: Продуктовый магазин", Toast.LENGTH_SHORT).show()
+        }
+
+        transferButton.setOnClickListener {
+            Toast.makeText(this, "Перевод: Выбор контакта...", Toast.LENGTH_SHORT).show()
+        }
+
+        // Так сделаем для кнопок "Пополнить" и "История" по аналогии
+        findViewById<Button>(R.id.btn_topup).setOnClickListener {
+            Toast.makeText(this, "Пополнение с карты", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.btn_more).setOnClickListener {
+            Toast.makeText(this, "Здесь будет история операций", Toast.LENGTH_SHORT).show()
         }
     }
 }
